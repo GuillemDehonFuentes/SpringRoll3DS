63675ac4be8f429275f96da3947118baf6c1c159
GuillemDehonFuentes <156358165+GuillemDehonFuentes@users.noreply.github.com>
2025-12-17 05:10:57 +0100

    Update character assets and add constant Z rotation script
    Replaced personajeV4.fbx with personajeV7.fbx and updated associated meta files. Renamed and updated materials and textures for character assets. Added RotacionConstanteZ script for constant Z-axis rotation. Updated scene and lighting data, and added new sprites and textures.

---
+++ Assets/Scripts/Components/RotacionConstanteZ.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RotacionConstanteZ : MonoBehaviour
+{
+    public float velocidad = 90f; // grados por segundo
+
+    void Update()
+    {
+        transform.Rotate(0f, 0f, velocidad * Time.deltaTime);
+    }
+}
